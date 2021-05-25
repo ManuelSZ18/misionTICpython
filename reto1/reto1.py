5f5b46ca1e7de0a3d921769b605d397d01ff2948
ManuelSZ18 <manuel_buitrago@hotmail.com>
2021-05-24 20:44:18 -0500

    Refactorizacion del Codigo

--- reto1/reto1.py
+++ reto1/reto1.py
@@ -1,22 +1,16 @@
-#RF01
 print('Bienvenido al sistema de ubicación para zonas públicas WIFI');
-#RF02
 userName = input('Nombre de Usuario: ');
-if userName != "51676": 
+if userName != "51676":
     print('Error');
 else:
     password = input('Contraseña: ');
-    if password != "67615":
+    if password != '67615':
         print('Error');
     else:
-        #RF03
-        inverseUserName = userName[::-1];
-        firstTerm = int(inverseUserName[0:3]);
-        secondTerm = (5%6) + (9%6) - 1;
+        firstTerm = int(password[0:3]);
+        secondTerm = (5%6) + (5%6) - (5-(1+1));
         captcha = int(input('Valor Captcha {} + {}: '.format(firstTerm, secondTerm)));
         if captcha != firstTerm + secondTerm:
             print('Error');
         else:
-            #RF04
-            print('Sesión iniciada');
-            
+            print('Sesión Iniciada'); 
