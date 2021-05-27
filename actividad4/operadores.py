90a8ba3aa542f578412eed910c5b68a38fc7d25d
ManuelSZ18 <manuel_buitrago@hotmail.com>
2021-05-27 10:25:03 -0500

    Refactorizando el codigo

--- actividad4/operadores.py
+++ actividad4/operadores.py
@@ -1,13 +1,12 @@
 import random
 indexOperations = 0;
 point = 1;
-for i in ['+', '-', '*', '/', '//', '%', '**', '>', '<', '<=', '+', '-', '*', '/', '//', '%', '**', '>', '<', '>=']:  
-    firstTerm = random.randrange(1, 10, 1);
-    secondTerm = random.randrange(1, 10, 1);   
-    operations = [firstTerm + secondTerm, firstTerm - secondTerm, firstTerm * secondTerm, firstTerm / secondTerm, firstTerm // secondTerm, 
-                  firstTerm % secondTerm, firstTerm ** secondTerm, firstTerm > secondTerm, firstTerm < secondTerm, firstTerm >= secondTerm, 
-                  firstTerm + secondTerm, firstTerm - secondTerm, firstTerm * secondTerm, firstTerm / secondTerm, firstTerm // secondTerm, 
-                  firstTerm % secondTerm, firstTerm ** secondTerm, firstTerm > secondTerm, firstTerm < secondTerm, firstTerm >= secondTerm];   
-    print('{}. La operación entre {} {} {} es: '.format(point, firstTerm, i, secondTerm) + str(operations[indexOperations]));
+for i in ['+', '-', '*', '/', '//', '%', '**', '>', '<', '<=', 
+          '+', '-', '*', '/', '//', '%', '**', '>', '<', '>=']:  
+    x = random.randrange(1, 10, 1);
+    y = random.randrange(1, 10, 1);   
+    operations = [x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y, 
+                  x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y];   
+    print('{}. La operación entre {} {} {} es: '.format(point, x, i, y) + str(operations[indexOperations]));
     indexOperations = indexOperations + 1;
     point = point + 1;    
