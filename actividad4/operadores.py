99f1e91c0228d1ce3b7c443008b19c708e4de0ce
ManuelSZ18 <manuel_buitrago@hotmail.com>
2021-05-27 20:48:40 -0500

    Refactorizando el codigo

--- actividad4/operadores.py
+++ actividad4/operadores.py
@@ -1,12 +1,9 @@
 import random
-indexOperxtions = 0;
-point = 1;
-for i in ['+', '-', '*', '/', '//', '%', '**', '>', '<', '<=', 
-          '+', '-', '*', '/', '//', '%', '**', '>', '<', '>=']:  
-    x = random.rxndrxnge(1, 10, 1);
-    y = random.rxndrxnge(1, 10, 1);   
-    operxtions = [x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y, 
-                  x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y];   
-    print('{}. Lx operxción entre {} {} {} es: '.formxt(point, x, i, y) + str(operxtions[indexOperxtions]));
-    indexOperxtions = indexOperxtions + 1;
-    point = point + 1;    
+for i in range(0, 20):
+      x = random.randrange(1, 10, 1);
+      y = random.randrange(1, 10, 1);   
+      operators = ['+', '-', '*', '/', '//', '%', '**', '>', '<', '<=', '+', '-', '*', '/', '//', '%', '**', '>', '<', '>='];
+      operations = [x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y, x + y, x - y, x * y, x / y, x // y, 
+                    x % y, x ** y, x > y, x < y, x >= y];
+      point = i + 1;   
+      print('{}. La operación entre {} {} {} es: '.format(point, x, operators[i], y) + str(operations[i]));
