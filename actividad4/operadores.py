1b186af6065e7b5c8e689d818dd62eb794bc51de
ManuelSZ18 <manuel_buitrago@hotmail.com>
2021-05-28 06:14:32 -0500

    refactoring

--- actividad4/operadores.py
+++ actividad4/operadores.py
@@ -3,7 +3,6 @@ for i in range(0, 20):
       x = random.randrange(1, 10, 1);
       y = random.randrange(1, 10, 1);   
       operators = ['+', '-', '*', '/', '//', '%', '**', '>', '<', '<=', '+', '-', '*', '/', '//', '%', '**', '>', '<', '>='];
-      operations = [x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y, x + y, x - y, x * y, x / y, x // y, 
-                    x % y, x ** y, x > y, x < y, x >= y];
-      point = i + 1;   
-      print('{}. La operación entre {} {} {} es: '.format(point, x, operators[i], y) + str(operations[i]));
+      operations = [x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y, x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y];
+      numeration = i + 1;   
+      print('{}. La operación entre {} {} {} es: '.format(numeration, x, operators[i], y) + str(operations[i]));
