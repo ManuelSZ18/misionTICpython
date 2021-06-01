ee7397f91add84879171913b10cbf7dddd3e4ac9
ManuelSZ18 <manuel_buitrago@hotmail.com>
2021-05-31 20:54:28 -0500

    refactorizacion de las operaciones aritmeticas

--- actividad4/operadores.py
+++ actividad4/operadores.py
@@ -1,8 +1,11 @@
 import random
-for i in range(0, 20):
-      x = random.randrange(1, 10, 1);
-      y = random.randrange(1, 10, 1);   
-      operators = ['+', '-', '*', '/', '//', '%', '**', '>', '<', '<=', '+', '-', '*', '/', '//', '%', '**', '>', '<', '>='];
-      operations = [x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y, x + y, x - y, x * y, x / y, x // y, x % y, x ** y, x > y, x < y, x >= y];
-      numeration = i + 1;   
-      print('{}. La operación entre {} {} {} es: '.format(numeration, x, operators[i], y) + str(operations[i]));
+numeration = 1;
+while numeration < 21:       
+      x = 1;
+      y = 1;
+      operations = {'+' : x + y, '-' : x - y, '*' : x * y, '/' : x / y, '**' : x ** y, '%' : x % y, '//' : x // y, '<' : x < y, '>' : x > y, '<=' : x <= y}     
+      for operation in operations:
+            x = random.randrange(1, 10, 1);
+            y = random.randrange(1, 10, 1);  
+            print ('{}. La operación entre {} {} {} es: {}'.format(numeration, x, operation, y, operations[operation]));            
+            numeration = numeration + 1; 
